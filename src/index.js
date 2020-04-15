17067ce4af73fff74a298d263f2c4e2df1774e4a
Ryan Lopopolo <rjl@hyperbo.la>
2020-04-14 22:46:04 -0700

    fmt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Favicons
 import "assets/favicons/android-chrome-192x192.png";
