3ade3161b2aa96afc3ef8344eb36ebe3d9585d5d
Ryan Lopopolo <rjl@hyperbo.la>
2020-05-12 08:04:23 -0700

    Remove highlight.js from install.js bundle

--- src/install.js
+++ src/install.js
@@ -1,17 +1,4 @@
 import "bootstrap";
 import "./bootstrap-slim.scss";
 
-import hljs from "highlight.js/lib/core";
 import "highlight.js/styles/default.css";
-import bash from "highlight.js/lib/languages/bash";
-import shell from "highlight.js/lib/languages/shell";
-
-hljs.registerLanguage("bash", bash);
-hljs.registerLanguage("console", shell);
-hljs.registerLanguage("shell", shell);
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("pre code").forEach((block) => {
-    hljs.highlightBlock(block);
-  });
-});
