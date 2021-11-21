600365743e2ae777fc6dc211bc695915f9877281
Ryan Lopopolo <rjl@hyperbo.la>
2021-11-20 17:18:46 -0800

    Remove webpack from site build process
    Replace webpack with a custom `build.mjs` script that copies static
    assets to the out dir, renders HTML templates with `eta` (including
    compiling markdown snippets to HTML with `marked`), and builds JS and
    CSS with `esbuild`.
    
    This PR collapses both `partials/nav/index.html` and
    `partials/nav/install.html` into `partials/nav.html` and passes in the
    current page via `eta.includeFile`.
    
    `index.scss` and `install.scss` are merged. `install.js` and
    `install.scss` are removed. Both pages depend on the same JS and CSS
    bundles.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,5 @@ import "./bootstrap-slim.scss";
 // marketing hero
 import "./index.scss";
 
-// Assets with well-known filenames
-import "@artichokeruby/logo/img/artichoke-logo.png";
-import "@artichokeruby/logo/img/artichoke-logo.svg";
-import "@artichokeruby/logo/img/artichoke-logo-inverted.png";
-import "@artichokeruby/logo/img/artichoke-logo-inverted.svg";
-import "@artichokeruby/logo/img/artichoke-social-logo.png";
-import "@artichokeruby/logo/img/artichoke-social-profile.png";
-import "./assets/robots.txt";
-
 // install snippets styles
 import "highlight.js/styles/default.css";
