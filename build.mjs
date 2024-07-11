7289205f36facb6f5879a6b7e30267fa9cd4d6cc
Ryan Lopopolo <rjl@hyperbo.la>
2024-07-10 17:19:53 -0700

    Remove minify-html from build pipeline
    It is not supported on Apple Silicon Macs and appears unmaintained.

--- build.mjs
+++ build.mjs
@@ -3,7 +3,6 @@ import { readFileSync } from "node:fs";
 import fs from "node:fs/promises";
 import path from "node:path";
 
-import minifyHtml from "@minify-html/node";
 import { Eta } from "eta";
 import esbuild from "esbuild";
 import hljs from "highlight.js";
@@ -114,19 +113,7 @@ const renderTemplate = async (template, locale) => {
   const eta = new Eta({ views: "src" });
   const content = eta.renderString(html, context);
 
-  const input = Buffer.from(content);
-  const output = minifyHtml.minify(input, {
-    do_not_minify_doctype: true,
-    ensure_spec_compliant_unquoted_attribute_values: true,
-    keep_closing_tags: true,
-    keep_html_and_head_opening_tags: true,
-    keep_spaces_between_attributes: true,
-    minify_js: true,
-    minify_css: true,
-    remove_bangs: false,
-  });
-
-  return output.toString();
+  return content;
 };
 
 const setupOutdir = async (outdir) => {
